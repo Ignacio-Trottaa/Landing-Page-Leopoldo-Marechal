e1369bfe5a8f48f3385304d64a5448fbfe03dd9c
nacho <nachotrotta81@gmail.com>
2024-06-17 15:04:38 -0300

    menu responsive para celulares/tablets

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const openMenuButton = document.getElementById("open-menu-button");
+    const closeMenuButton = document.getElementById("close-menu-button");
+    const mobileMenu = document.getElementById("mobile-menu");
+
+    openMenuButton.addEventListener("click", function () {
+        mobileMenu.classList.remove("hidden");
+    });
+
+    closeMenuButton.addEventListener("click", function () {
+        mobileMenu.classList.add("hidden");
+    });
+});
